bd0889151460a2ab4d1b4f065abf31d0bfc80d9c
David <daloalga2@gmail.com>
2015-04-11 22:37:38 +0200

    Con auxiliares

---
+++ App_Code/BD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class BD
+{
+    public static string String(){
+        return "Data Source=C:\\Users\\David\\Documents\\Visual Studio 2010\\WebSites\\WebSite2\\App_Data\\BaseDeDatos.sdf";
+    }
+}
