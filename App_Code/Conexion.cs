793c3c30174ed69e2f94fade1b0e6596b40251ef
David <daloalga2@gmail.com>
2015-04-11 20:14:03 +0200

    Base

---
+++ App_Code/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+/// <summary>
+/// Descripción breve de Conexion
+/// </summary>
+public class Conexion{
+    private SqlDataAdapter adaptador = new SqlDataAdapter();
+    private SqlConnection conexion = new SqlConnection();
+    private SqlCommand comandos= new SqlCommand();
+    public Conexion(){
+        comandos.Connection = conexion;
+        adaptador.SelectCommand = comandos;
+        conexion.ConnectionString = BD.String();
+	}
+    public DataSet ejecutar(string comando){
+        DataSet contenido = new DataSet();
+        comandos.CommandText = comando;
+        try { conexion.Open(); }catch(SqlException ex){}
+        adaptador.Fill(contenido,"cosas");
+        conexion.Close();
+        return contenido;
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "hola";
+        Conexion c = new Conexion();
+        DataSet data=c.ejecutar("SELECT * FROM USUARIOS");
+        DataTable tabla=data.Tables["cosas"];
+        Label1.Text = tabla.Rows[0].ToString();
+    }
+}
