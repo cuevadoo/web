f197d8cf81f9bce3b888ebd791e04f1794a70977
cuevadooLeo <leorodriguezmrez@gmail.com>
2015-04-24 19:51:41 +0200

    CAD.MensajeDifusion estructura base hecho

--- App_Code/CAD/MensajeDifusion.cs
+++ App_Code/CAD/MensajeDifusion.cs
@@ -10,5 +10,22 @@ namespace CAD
 {
     public class MensajeDifusion
     {
+        private static Conexion conexion = new Conexion();
+        public void create(EN.MensajeDifusion mDifusion)
+        {
+
+        }
+        public void delete(EN.MensajeDifusion mDifusion)
+        {
+
+        }
+        public EN.MensajeDifusion read(String email)
+        {
+            return null;
+        }
+        public void update(EN.MensajeDifusion deleted, EN.MensajeDifusion added)
+        {
+
+        }
     }
 }
