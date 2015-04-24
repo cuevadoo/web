55ee58b75a59d6fd6fbacfe81b5a86a0a06527e1
Adrián Tejedor Giménez <adri_teke@hotmail.com>
2015-04-24 18:34:41 +0200

    Creacion del CAD y EN de GustosFilm

--- App_Code/EN/GustosFilm.cs
+++ App_Code/EN/GustosFilm.cs
@@ -8,7 +8,70 @@ using System.Web;
 /// </summary>
 namespace EN
 {
-    public class GustosFilm
+    public class GustosFilm:Usuario
     {
+        private String genero;
+        private String director;
+        private byte decada;
+        private String actor;
+        private String pelicula;
+        private String Sgenero;
+        private String Sdirector;
+        private String serie;
+
+        public GustosFilm(String genero,String director,byte decada,String actor,String pelicula,String Sgenero,String Sdirector,String serie){
+            this.genero = genero;
+            this.director = director;
+            this.decada = decada;
+            this.actor = actor;
+            this.pelicula = pelicula;
+            this.Sgenero = Sgenero;
+            this.Sdirector = Sdirector;
+            this.serie = serie;
+        }
+        
+
+        public String Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+        public String Director
+        {
+            get { return director; }
+            set { director = value; }
+        }
+        public byte Decada
+        {
+            get { return decada; }
+            set { decada = value; }
+        }
+        public String Actor
+        {
+            get { return actor; }
+            set { actor = value; }
+        }
+        public String Pelicula
+        {
+            get { return pelicula; }
+            set { pelicula = value; }
+        }
+        public String S_genero1
+        {
+            get { return Sgenero; }
+            set { Sgenero = value; }
+        }
+
+        public String S_director1
+        {
+            get { return Sdirector; }
+            set { Sdirector = value; }
+        }
+        public String Serie
+        {
+            get { return serie; }
+            set { serie = value; }
+        }
+
     }
 }
