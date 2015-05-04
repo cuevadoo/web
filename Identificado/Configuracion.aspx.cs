838a874e93e4e127b721a3c4ebb6dd6b7f1f6010
Adrián Tejedor Giménez <adri_teke@hotmail.com>
2015-05-04 17:02:41 +0200

    Configuracion.aspx.cs cambiado y guardada la foto de perfil predeterminada

--- Identificado/Configuracion.aspx.cs
+++ Identificado/Configuracion.aspx.cs
@@ -8,6 +8,9 @@ using System.Web.UI.WebControls;
 public partial class Identificado_Configuracion : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e){
-
+        EN.Usuario user=(EN.Usuario)Session["User"];
+        user.foto = LabelFoto;
     }
+
+
 }
