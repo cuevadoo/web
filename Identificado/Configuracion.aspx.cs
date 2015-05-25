721c7214d4d0c23ff0e61a851882cfcc7679489c
Adrián Tejedor Giménez <adri_teke@hotmail.com>
2015-05-26 01:10:37 +0200

    Configuracion.aspx terminado. Ya no hay errores de base de datos

--- Identificado/Configuracion.aspx.cs
+++ Identificado/Configuracion.aspx.cs
@@ -40,41 +40,57 @@ public partial class Identificado_Configuracion : System.Web.UI.Page
                 {
                     new CAD.Usuario().update(user, cambios);
                     //Actualización de gustos
-                    EN.GustosFilm gf = new CAD.GustosFilm().read(user.Email);                   
-                    gf.Email = cambios.Email;
-                    EN.GustosFilm gfv = new GustosFilm(null,null,0,null,null,null,null,null,user.Email);
-                    new CAD.GustosFilm().update(gfv, gf);
+                    try
+                    {
+                        EN.GustosFilm gf = new CAD.GustosFilm().read(user.Email);
+                        gf.Email = cambios.Email;
+                        EN.GustosFilm gfv = new GustosFilm(null, null, 0, null, null, null, null, null, user.Email);
+                        new CAD.GustosFilm().update(gfv, gf);
+                    }
+                    catch(CAD.Exception ex) { }
 
-                    EN.GustosMusicales gm = new CAD.GustosMusicales().read(user.Email);
-                    gm.Email = cambios.Email;
-                    EN.GustosMusicales gmv = new GustosMusicales(null,null,null,null,0,user.Email);
-                    new CAD.GustosMusicales().update(gmv, gm);
+                    try
+                    {
+                        EN.GustosMusicales gm = new CAD.GustosMusicales().read(user.Email);
+                        gm.Email = cambios.Email;
+                        EN.GustosMusicales gmv = new GustosMusicales(null, null, null, null, 0, user.Email);
+                        new CAD.GustosMusicales().update(gmv, gm);
+                    }
+                    catch (CAD.Exception ex) { }
 
-                    EN.GustosOrdenadores gi = new CAD.GustosOrdenadores().read(user.Email);
-                    gi.Email = cambios.Email;
-                    EN.GustosOrdenadores giv = new GustosOrdenadores(null, null, null, user.Email);
-                    new CAD.GustosOrdenadores().update(giv, gi);
+                    try
+                    {
+                        EN.GustosOrdenadores gi = new CAD.GustosOrdenadores().read(user.Email);
+                        gi.Email = cambios.Email;
+                        EN.GustosOrdenadores giv = new GustosOrdenadores(null, null, null, user.Email);
+                        new CAD.GustosOrdenadores().update(giv, gi);
+                    }
+                    catch (CAD.Exception ex) { }
 
-                    EN.GustosVideojuegos gv = new CAD.GustosVideojuegos().read(user.Email);
-                    gv.Email = cambios.Email;
-                    EN.GustosVideojuegos gvv = new GustosVideojuegos(null, null, null, null, user.Email);
-                    new CAD.GustosVideojuegos().update(gvv, gv);
-
-                    EN.Relaciones re = new CAD.Relaciones().read(user.Email);
-                    EN.Relaciones rev = new CAD.Relaciones().read(user.Email);
-                    re.Usuario1 = cambios.Email;
-                    new CAD.Relaciones().update(rev, re);
-
-                    EN.Residencia res = new CAD.Residencia().read(user.Email);
-                    res.Email = cambios.Email;
-                    EN.Residencia resv = new Residencia(null, null, null, user.Email);
-                    new CAD.Residencia().update(resv,res);
-
-
-
-
-
-
+                    try
+                    {
+                        EN.GustosVideojuegos gv = new CAD.GustosVideojuegos().read(user.Email);
+                        gv.Email = cambios.Email;
+                        EN.GustosVideojuegos gvv = new GustosVideojuegos(null, null, null, null, user.Email);
+                        new CAD.GustosVideojuegos().update(gvv, gv);
+                    }
+                    catch (CAD.Exception ex) { }
+                    try
+                    {
+                        EN.Relaciones re = new CAD.Relaciones().read(user.Email);
+                        EN.Relaciones rev = new CAD.Relaciones().read(user.Email);
+                        re.Usuario1 = cambios.Email;
+                        new CAD.Relaciones().update(rev, re);
+                    }
+                    catch (CAD.Exception ex) { }
+                    try
+                    {
+                        EN.Residencia res = new CAD.Residencia().read(user.Email);
+                        res.Email = cambios.Email;
+                        EN.Residencia resv = new Residencia(null, null, null, user.Email);
+                        new CAD.Residencia().update(resv, res);
+                    }
+                    catch (CAD.Exception ex) { }
 
                     LabelError.Text = "Cambios guardados correctamente";
                     Session["User"] = cambios;
